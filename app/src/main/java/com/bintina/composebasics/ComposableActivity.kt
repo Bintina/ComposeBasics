9936ca6acd08925cc3ebc20b5052bbb1389b9ec6
Bintina <105106238+Bintina@users.noreply.github.com>
2023-11-22 09:19:03 +0300

    A little closer
    Only a little closer

--- app/src/main/java/com/bintina/composebasics/ComposableActivity.kt
+++ app/src/main/java/com/bintina/composebasics/ComposableActivity.kt
@@ -1,26 +1,20 @@
 package com.bintina.composebasics
 
+//import androidx.compose.foundation.layout.ColumnScopeInstance.weight
+//import androidx.compose.foundation.layout.RowScopeInstance.weight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.FloatRange
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScopeInstance.alignBy
-import androidx.compose.foundation.layout.ColumnScopeInstance.weight
-//import androidx.compose.foundation.layout.ColumnScopeInstance.weight
+import androidx.compose.foundation.layout.ColumnScope
+
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScopeInstance.alignBy
-//import androidx.compose.foundation.layout.RowScopeInstance.weight
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.RowScope
+
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,8 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.bintina.composebasics.ui.theme.ComposeBasicsTheme
 
 class ComposableActivity : ComponentActivity() {
@@ -53,90 +47,21 @@ class ComposableActivity : ComponentActivity() {
     @Composable
     private fun ComposableQuadrant(modifier: Modifier = Modifier) {
         Box(
-            modifier = Modifier
-                .size(80.dp, 40.dp)
-                .alignBy{it.measuredWidth/2}
-                .alignBy{it.measuredHeight/2}
-                .padding(16.dp)
+            modifier = Modifier.fillMaxSize()
         ) {
-//Top Left
-            QuadrantContainer(
-                modifier = Modifier.weight(weight =
-                )
-            )
-            {
-                TextArticle()
-            }
-            QuadrantContainer(
-                modifier = Modifier.fillMaxSize()
-            )
-            {
-                ImageArticle()
-            }
-            QuadrantContainer(
-                modifier = Modifier.fillMaxSize()
-            )
-            {
-                RowArticle()
-            }
-            QuadrantContainer(
-                modifier = Modifier.fillMaxSize()
-            )
-            {
-                ColumnArticle()
+            Column(
+                Modifier
+            ) {
+
             }
         }
-        /*
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.fillMaxWidth(fraction = 0.5F)
-                ) {
-                    Row(verticalAlignment = Alignment.Top,
-                        modifier = Modifier.fillMaxHeight(fraction = 0.5F)) {
-                            TextArticle()
-                    }
-                    Row(verticalAlignment = Alignment.Bottom,
-                        modifier = Modifier.fillMaxHeight(fraction = 0.5F)) {
-                            ImageArticle()
-                    }
-                }
-                Column(
-                    horizontalAlignment = Alignment.End,
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    modifier = Modifier.fillMaxWidth(fraction = 0.5F)
-                ) {
-                    Row(verticalAlignment = Alignment.Top,
-                        modifier = Modifier.fillMaxHeight(fraction = 0.5F)) {
-                            RowArticle()
-                    }
-                    Row(verticalAlignment = Alignment.Bottom,
-                        modifier = Modifier.fillMaxHeight(fraction = 0.5F)) {
-                            ColumnArticle()
-                    }
-                }
-        */
-
-
     }
 
-    @Composable
-    fun QuadrantContainer(
-        modifier: Modifier = Modifier,
-        content: @Composable BoxScope.() -> Unit
-    ) {
-        Box(
-            modifier = modifier,
-            contentAlignment = Alignment.Center,
-            content = content
-        )
-
-    }
 
     @Composable
     fun TextArticle() {
         Column(modifier = Modifier.background(color = colorResource(id = R.color.text_quadrant))) {
-            Text(text = "Text composable")
+            Text(text = "Text composable", fontWeight = FontWeight.Bold)
             Text(text = "Displays text and follows the recommended Material Design guidelines.")
         }
     }
@@ -144,7 +69,7 @@ class ComposableActivity : ComponentActivity() {
     @Composable
     fun ImageArticle() {
         Column(modifier = Modifier.background(color = colorResource(id = R.color.image_quadrant))) {
-            Text(text = "Image composable")
+            Text(text = "Image composable", fontWeight = FontWeight.Bold)
             Text(text = "Creates a composable that lays out and draws a given Painter class object.")
         }
     }
@@ -152,7 +77,7 @@ class ComposableActivity : ComponentActivity() {
     @Composable
     fun RowArticle() {
         Column(modifier = Modifier.background(color = colorResource(id = R.color.row_quadrant))) {
-            Text(text = "Row composable")
+            Text(text = "Row composable", fontWeight = FontWeight.Bold)
             Text(text = "A layout composable that places its children in a horizontal sequence.")
         }
     }
@@ -160,15 +85,51 @@ class ComposableActivity : ComponentActivity() {
     @Composable
     fun ColumnArticle() {
         Column(modifier = Modifier.background(color = colorResource(id = R.color.column_quadrant))) {
-            Text(text = "Column composable")
+            Text(text = "Column composable", fontWeight = FontWeight.Bold)
             Text(text = "A layout composable that places its children in a vertical sequence.")
+        }
+    }
+
+    @Composable
+    fun BoxWithRowLayout(content: @Composable RowScope.() -> Unit) {
+        Row {
+            content()
+        }
+    }
+
+    @Composable
+    fun BoxWithColumnLayout(content: @Composable ColumnScope.() -> Unit) {
+        Column {
+            content()
         }
     }
 
     @Preview
     @Composable
     fun QuadrantPreview() {
-        ComposableQuadrant()
+        BoxWithColumnLayout() {
+            Column(Modifier.weight(0.5f, true)) {
+
+                BoxWithRowLayout() {
+                    Row(Modifier.weight(0.5f)) {
+                        TextArticle()
+                    }
+                    Row(Modifier.weight(0.5f)) {
+                        ImageArticle()
+                    }
+                }
+            }
+            Column(Modifier.weight(0.5f, true)) {
+                BoxWithRowLayout {
+                    Row(Modifier.weight(0.5f)) {
+                        ColumnArticle()
+                    }
+                    Row(Modifier.weight(0.5f)) {
+                        RowArticle()
+                    }
+                }
+            }
+        }
     }
 }
 
